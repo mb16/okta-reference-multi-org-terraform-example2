397487324d42994a0cb56ad09556c8a76d577e24
mb16 <mb16@users.noreply.github.com>
2023-02-22 21:11:36 -0500

    added space

--- grouprules.tf
+++ grouprules.tf
@@ -4,7 +4,7 @@ resource "okta_group_rule" "dmzadmin_grouprule" {
   group_assignments = [ resource.okta_group.dmzadmin_group.id ]
   expression_type   = "urn:okta:expression:1.0"
   expression_value  = "user.department==\"DMZ Ops\""
-}
+} 
 
 resource "okta_group_rule" "test_rule" {
   name              = "Test Rule"
